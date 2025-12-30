57c0edb73df8b413b1774abcbee81f8b7c56eaae
ab2882489-hue <ab2882489@gmail.com>
2025-12-30 20:23:35 +0500

    Add Footer component with styling and copyright

---
+++ app/components/Footer.js
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer style={{
+      marginTop: "60px",
+      padding: "20px",
+      background: "#f2f2f2",
+      textAlign: "center",
+      fontSize: "16px"
+    }}>
+      © 2025 Maria Bibi. All Rights Reserved.
+    </footer>
+  )
+}
